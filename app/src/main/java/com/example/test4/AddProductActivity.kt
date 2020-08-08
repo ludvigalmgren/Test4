b9c6ab8a28b6f1aace16b78949acdae0f4d40966
ludvigalmgren <ludvig.almgren@hotmail.com>
2020-08-08 19:47:20 +0200

    Ver4 Release

--- app/src/main/java/com/example/test4/AddProductActivity.kt
+++ app/src/main/java/com/example/test4/AddProductActivity.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.add_product.*
 import java.util.*
 
@@ -25,51 +26,71 @@ class AddProductActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_product)
 
-        //Initiate database reference
-        val ref = FirebaseDatabase.getInstance().getReference("scoring")
-
-        //Initiate dialogue
-        val alertDialogBuilder = AlertDialog.Builder(this)
-        alertDialogBuilder.setMessage("You can only vote once per day")
+        //Initiate dialogue for only one vote
+        val alertOnlyOneVote = AlertDialog.Builder(this)
+        alertOnlyOneVote.setMessage("You can only vote once per day")
             .setNegativeButton("Ok!", DialogInterface.OnClickListener { dialog, id ->
                 dialog.cancel()
-                startActivity(Intent(this, MainActivity::class.java))
             })
 
-        //Get the date and the time
+        //Initiate dialogue for max 10
+        val alertMaxTen = AlertDialog.Builder(this)
+        alertMaxTen.setMessage("Score must be 1-10")
+            .setNegativeButton("Ok!", DialogInterface.OnClickListener { dialog, id ->
+                dialog.cancel()
+            })
+
+        //Get the date
         val c = Calendar.getInstance()
         val day = c[Calendar.DAY_OF_MONTH]
         val month = c[Calendar.MONTH]+1
         val dayAndMonth = day.toString().plus("/").plus(month.toString())
-        val hour = c[Calendar.HOUR_OF_DAY]
-        val minute = c[Calendar.MINUTE]
-        val hourMinute = hour.toString().plus(":").plus(minute.toString())
+
+        //Initiate Firebase database reference
+        val ref = FirebaseDatabase.getInstance().getReference("scoring")
 
         //Display the date in the top
         displayDailyDate.text =dayAndMonth
 
-
         //Set today's picture
         imageViewSquirrel.setImageResource(allPics[day-1])
 
-        //Store the value submitted as a shared preference
+        //Display your score
+        textViewYourScoreAddProduct.text = myScores[day-1].toString()
+
+        //Display average score
+        textViewAverageAddProduct.text = allAverages[day-1].toString().take(3)
+
+        //Store the value submitted when submit button pushed
         addRatingSubmitButton.setOnClickListener {
-            if (countScores >= 1 ){
-                alertDialogBuilder.show()
-            }else {
-                    countScores += 1
-                    ref.child(hourMinute).setValue(cuteness.text.toString())
-                    val hej = cuteness.text.toString()
+            val hej = cuteness.text.toString()
+            if(hej.isNotEmpty()) {
+                val hej2 = hej.toInt()
+                if (countMyScores >= 1) {
+                    alertOnlyOneVote.show()
+                } else if (hej2 > 10 || hej2 < 1 ) {
+                    alertMaxTen.show()
+                } else {
+                    //Get time
+                    val day = c[Calendar.DAY_OF_MONTH]
+                    //Write value to database
+                    val updateScore = allAverages[day-1] * countAllScores + hej2
+                    ref.child(day.toString()).child("Sum").setValue(updateScore)
+                    countAllScores += 1
+                    countMyScores += 1
+                    //Write count to database
+                    ref.child(day.toString()).child("Count").setValue(countAllScores)
+                    //Write value to myScores
                     myScores[day - 1] = hej.toInt()
-                Log.d("Ludvig", "countScores: $countScores")
-                startActivity(Intent(this, MainActivity::class.java))
+                    startActivity(Intent(this, MainActivity::class.java))
+                }
             }
         }
-
-
+        //Go back when Back button pushed
+        buttonBack.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
 
     }
-
-
 
 }
--- app/src/main/java/com/example/test4/MainActivity.kt
+++ app/src/main/java/com/example/test4/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
-import java.util.Timer
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
@@ -16,11 +16,11 @@ import java.util.*
 var myScores= mutableListOf<Int>(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 
-var allScores= mutableListOf<Int>(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+var allAverages= mutableListOf<Double>(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                     0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
 
-var count = 1
-var countScores = 0
+var countAllScores = 0
+var countMyScores = 0
 
 class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.N)
@@ -37,58 +37,53 @@ class MainActivity : AppCompatActivity() {
         val c = Calendar.getInstance()
         val day = c[Calendar.DAY_OF_MONTH]
 
-        //Zero count and countscore
+        //Initiate Firebase database reference
+        val ref = FirebaseDatabase.getInstance().getReference("scoring")
+
+        //Do every minute
         Timer().scheduleAtFixedRate(object : TimerTask() {
             override fun run() {
-                val hour = c[Calendar.HOUR_OF_DAY]
-                val minute = c[Calendar.MINUTE]
-                //At 00.00 reset count and countScores
-                d("Ludvig","hour: $hour")
-                d("Ludvig","minute: $minute")
-                if(hour == 0 && minute == 0){
-                    count=1
-                    countScores=0
-                }
+                val c2 = Calendar.getInstance()
+                val hour = c2[Calendar.HOUR_OF_DAY]
+                val minute = c2[Calendar.MINUTE]
+                    //Zero countAllScores and countMyScores if midnight
+                    if(hour == 0 && minute == 0){
+                        countMyScores=0
+                    }
+                //Take value from database when a new value appears
+                val menuListener = object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                //Take value from database for entire month
+                    for (i in 1..31) {
+                        val databaseScore = dataSnapshot.child("$i").child("Sum").value
+                        if (databaseScore != null) {
+                            val score = databaseScore.toString()
+                            val score2 = score.toDouble()
+                            countAllScores =
+                                dataSnapshot.child("$i").child("Count").value.toString().toInt()
+                            allAverages[i - 1] = score2 / countAllScores.toDouble()
+                        }
+                    }
+                    d("Ludvig", "allAverages: $allAverages")
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+               // handle error
+            }
+        }
+        ref.addListenerForSingleValueEvent(menuListener)
             }
         },0, 60000)
 
+        //Update the picture and the text fields
+        val maxAverage = allAverages.max()
+        val highScoreIndex = allAverages.indexOf(maxAverage)
+        imageHighScoreSquirrel.setImageResource(allPics[highScoreIndex])
+        viewAverage.text = allAverages[highScoreIndex].toString().take(3)
+        viewYourScore.text = myScores[highScoreIndex].toString()
 
-
-        //Take value from database when a new value appears
-        val menuListener = object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                var databaseScore = dataSnapshot.value
-                val score = databaseScore.toString()
-                d("Ludvig","score: $score")
-                if(score.isNullOrEmpty()) {
-                }else{
-                    val score2 = score.toInt()
-                    d("Ludvig","databasevalue: $score2")
-                    allScores[day - 1] += score2
-                    val averageScore = allScores[day-1].toDouble()/count.toDouble()
-                    count += 1
-                    d("Ludvig","count: $count")
-                    d("Ludvig","sumt: $allScores")
-                    viewAverage.text = averageScore.toString()
-                }
-            }
-            override fun onCancelled(databaseError: DatabaseError) {
-     //           // handle error
-            }
-        }
-
-        d("Ludvig","day: $day")
-        //Pick out the highest score
-        var highScore = myScores.max()
-        viewHighScore.text = highScore.toString()
-        var averageScore = allScores.max()
-
-        //Display the image of the leader
-        val highScoreIndex = allScores.indexOf(averageScore)
-        if (highScore != null && highScore.toInt() == 0){
-            imageHighScoreSquirrel.setImageResource(R.drawable.icon)
-        }else {
-            imageHighScoreSquirrel.setImageResource(allPics[highScoreIndex])
+        //Display icon if all scores are zero
+        if(allAverages.max() == 0.0) {
+           imageHighScoreSquirrel.setImageResource(R.drawable.icon)
         }
 
     }
